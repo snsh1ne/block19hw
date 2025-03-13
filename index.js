735d1c2872a3519e71af814a447c8cb39d6aa4c9
Sara Yared <sarayared@gmail.com>
2025-03-13 19:07:34 -0400

    add dynamic price calculate element

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const pets = [
   { name: "Dr. Pressure", price: 51, occupation: "programmer" },
   { name: "Prof. Possibility", price: 43, occupation: "teacher" },
   { name: "Prof. Prism", price: 81, occupation: "teacher" },
+];
+
+const newPets = [
   { name: "Dr. Impulse", price: 43, occupation: "teacher" },
   { name: "Prof. Spark", price: 76, occupation: "programmer" },
   { name: "Dr. Wire", price: 47, occupation: "teacher" },
@@ -18,21 +21,28 @@ function init() {
   h1.innerHTML = "Freelancer Forum";
   root.append(h1);
 
+  const h2 = document.createElement("h2");
+  h2.id = "avgPrice";
+  root.append(h2);
+
   const petsContainer = document.createElement("div");
   petsContainer.id = "petsContainer";
   petsContainer.classList.add("pets");
-  petsContainer.classList.add("dogs");
-  petsContainer.classList.remove("dogs");
   root.append(petsContainer);
 
   renderPets();
 } //end init
 
-
 function renderPets() {
   //targeted the petsContaier created in the init function
   const container = document.querySelector("#petsContainer");
   container.innerHTML = "";
+
+  function calculateAveragePrice() {
+    const total = pets.reduce((num, pet) => num + pet.price, 0);
+    const average = total / pets.length;
+    return average.toFixed(2); // Limit to 2 decimal places
+  }
 
   //looped over our pet array
   pets.forEach((pet) => {
@@ -48,15 +58,25 @@ function renderPets() {
 
     container.append(petDiv);
   });
+
+  const avgPrice = document.querySelector("#avgPrice");
+  avgPrice.innerHTML = `<h2>Average Starting Price: $${calculateAveragePrice()}</h2>`;
 } //end renderPets()
 
-//add new pet to the array
 const addPet = () => {
   const randomIndex = Math.floor(Math.random() * pets.length);
   console.log(randomIndex);
-  const pets = pets[randomIndex];
+  //const newPet variable then its calling the array newPets....
+  const newPet = newPets[randomIndex];
   pets.push(newPet);
+  renderPets();
 };
+
+const add = setInterval(addPet, 1000);
+
+setTimeout(() => {
+  clearInterval(add);
+}, 10000);
 
 //call init function
 init();
